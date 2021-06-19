67a518678d053ff6c16558bf5ad56775b2c5993a
Chris Bailey <cjbailey200982@gmail.com>
2021-06-19 07:45:51 +0100

    Initial Commit

---
+++ Module6Challenge/Module6ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module6ChallengeApp.swift
+//  Module6Challenge
+//
+//  Created by Boosh on 19/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Module6ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
